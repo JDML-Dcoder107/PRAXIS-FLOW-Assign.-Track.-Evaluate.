c805d5f0a858c2e51341d027be706486f42bdda3
JDML-Dcoder107 <24-02573@g.batstate-u.edu.ph>
2025-11-09 10:17:25 +0800

    initcommit

---
+++ src/PersonalInformation.java
@@ -0,0 +1,17 @@
+//Encapsulation
+public class PersonalInformation {
+    private String firstName;
+    private String lastName;
+    private String middleInitial;
+    private int aGe;
+    private String posiTion;
+
+    //setter
+    public PersonalInformation(String firstName, String lastName, String middleInitial, int aGe, String posiTion){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleInitial = middleInitial;
+        this.aGe = aGe;
+        this.posiTion = posiTion;
+    }
+}
